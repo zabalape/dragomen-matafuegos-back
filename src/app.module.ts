64dda82c8f5cd70ed1db2f5f5d4eca3b6e5eae47
GabrielZabala <zabalapedro0@gmail.com>
2026-05-01 18:07:34 -0300

    config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { DirectusModule } from './directus/directus.module';
 import { LeadsModule } from './leads/leads.module';
 import { ProductsModule } from './products/products.module';
 
+
 @Module({
   imports: [DirectusModule, ProductsModule, ContentModule, LeadsModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
