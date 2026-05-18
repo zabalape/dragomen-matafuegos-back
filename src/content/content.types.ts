fde617a70ba35cb1aa10723883bfb277a2b3bbbb
Gabriel Zabala <88402616+zabalape@users.noreply.github.com>
2026-05-17 22:15:25 -0300

    Update content.types.ts

--- src/content/content.types.ts
+++ src/content/content.types.ts
@@ -4,6 +4,8 @@ export type PublicacionBlog = {
   titulo: string;
   resumen: string;
   fechaPublicacion: string;
+  categoria?: string; // 👈 Agregado opcional para la etiqueta (ej. "Matafuegos")
+  imagen?: string;    // 👈 Agregado opcional para el UUID de la foto de Directus
 };
 
 export type Testimonio = {
